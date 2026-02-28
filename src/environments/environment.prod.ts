fcdc16ae853504d9e5b3c32997ae600ac58ec783
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2026-03-01 00:29:47 +0300

    [fix] - Environment fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://m1p13mean-nyavo-fitia-api.up.railway.app/api/v1',
-  firebase: {
-    apiKey: "AIzaSyApmJ1WmFwzdoLUnw7iSyNxXascT-yp4zY",
-    authDomain: "social-media-commerce.firebaseapp.com",
-    projectId: "social-media-commerce",
-    storageBucket: "social-media-commerce.appspot.com",
-    messagingSenderId: "103776198855",
-    appId: "1:103776198855:web:something"
-  }
+  apiUrl: '/api'
 };
